d0cc1844458cd3560e3a513b41c8e0446b5cc196
efi17 <elisabeth.fittschen@gmail.com>
2019-05-07 11:09:44 +0200

    Update Block.py

--- Block.py
+++ Block.py
@@ -1,1 +1,14 @@
 # the Block ~~~ MAGIC ~~~
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+
+class Block:
+    def __init__(self, data, prev = None):
+        self.data = data
+        self.prev = prev
+        self.next = None
+
+    def set_next(self, next):
+        self.next = next
+
