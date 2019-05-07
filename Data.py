fe7605d4e0eaf737b3f55bf682f5beedc40999af
efi17 <elisabeth.fittschen@gmail.com>
2019-05-07 11:10:53 +0200

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,2 +1,13 @@
 # The Data ~~~ Normal!? ~~~
+class Data:
+    def __init__(self, creatorID, objectID, objectType, pfand, status, prevhash):
+        self.creatorID = creatorID
+        self.objectID = objectID
+        self.objectType = objectType
+        self.pfand = pfand
+        self.status = status
+        self.prevhash = prevhash
 
+    def __str__(self):
+        return str(self.creatorID) + str(self.objectID) + str(self.objectType) + \
+               str(self.pfand) + str(self.status) + str(self.prevhash)
