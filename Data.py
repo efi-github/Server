41f5b3eda69ec032be570e7c954ee1f5e9bcc3fc
efi17 <elisabeth.fittschen@gmail.com>
2019-05-12 21:11:16 +0200

    Update Data.py

--- Data.py
+++ Data.py
@@ -2,13 +2,12 @@ from uuid import uuid4
 
 # The Data ~~~ Normal!? ~~~
 class Data:
-    def __init__(self, creatorID, objectType, status, prevhash):
+    def __init__(self, creatorID, objectType, status):
         self.creatorID = creatorID
         self.objectID = uuid.uuid4()
         self.objectType = objectType
         self.pfand = 12
         self.status = status
-        self.prevhash = prevhash
 
     def __str__(self):
         return str(self.creatorID) + str(self.objectID) + str(self.objectType) + \
