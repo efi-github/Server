5b8f8056af62acf3b36e729a2bad6299bd896087
elisabethfittschen <elisabeth.fittschen@tu-clausthal.de>
2019-05-27 11:10:19 +0200

    uuid added

--- server/blockchain/views.py
+++ server/blockchain/views.py
@@ -1,4 +1,5 @@
 import json
+from uuid import uuid4
 from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
@@ -8,7 +9,7 @@ from .serializers import BlockSerializer
 
 class BlockView(APIView):
     serializer_class = BlockSerializer
-
+    head_objectID = [0]
     def get_object(self, uuid):
         try:
             return Block.objects.get(objectID=uuid)
@@ -16,8 +17,9 @@ class BlockView(APIView):
             raise Http404
 
     def get(self, request, uuid):
-        if not uuid:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        if uuid == "0":
+            uuid = self.head_objectID[0]
+            print(self.head_objectID[0])
 
         block = self.get_object(uuid)
         serializer = BlockSerializer(block, many=False)
@@ -30,15 +32,17 @@ class BlockView(APIView):
             or not "objectType" in body
             or not "pfand" in body):
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        
+        new_objectID = uuid4()
         Block.objects.create(
                         creatorID = body["creatorID"],
-                        objectID = body['objectID'],
+                        objectID = new_objectID,
                         objectType = body['objectType'],
                         pfand = body['pfand'],
                         status = "In Benutzung",
+                        prevID = head_objectID[0],
                         prevhash = "foo")
-        return Response(status=status.HTTP_200_OK)
+        self.head_objectID[0] = new_objectID
+        return Response(str(self.head_objectID[0]), status=status.HTTP_200_OK)
 
     def put(self, request):
         body = json.loads(request.body)
