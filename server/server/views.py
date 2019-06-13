fe72ff71c790d6ad49af65028a0fae5876f15391
elisabethfittschen <elisabeth.fittschen@tu-clausthal.de>
2019-06-13 18:23:21 +0200

    clean up

--- server/server/views.py
+++
@@ -1,7 +0,0 @@
-
-
-# def respond_http(request,qr=""):
-#     if qr == "3fa06618-9aa8-41b5-a65e-a102f2309d88":
-#         return HttpResponse("{\r\n\"Type\": \"Mobile Phone\",\r\n\"Manufacturer\": \"Samsung\",\r\n\"Name\": \"Galaxy S7\",\r\n\"Deposit\": 20\r\n}", status=200)
-#     else:
-#         return HttpResponse('Does not exist',status=404)
